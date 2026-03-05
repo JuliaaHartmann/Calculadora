324cf47cb313215f747841eea8ba46f736ad377f
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-03-05 14:43:59 -0300

    Adicionar checagem numero NullOrEmpty

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -23,7 +23,7 @@ while (deveContinuar == true) // condição
     Console.WriteLine();
 
     Console.WriteLine("Selecione uma opção válida: ");
-    string operacaoSelecionada = Console.ReadLine();
+    string? operacaoSelecionada = Console.ReadLine();
 
     if (operacaoSelecionada == "S")
     {
@@ -33,17 +33,38 @@ while (deveContinuar == true) // condição
     }
 
     Console.WriteLine("Digite o primeiro número: ");
-    string strPrimeiroNumero = Console.ReadLine();
+    string? strPrimeiroNumero = Console.ReadLine();
 
     Console.WriteLine();
 
     Console.WriteLine("Digite o segundo número: ");
-    string strSegundoNumero = Console.ReadLine();
+    string? strSegundoNumero = Console.ReadLine();
 
     Console.WriteLine("O primeiro número digitado foi: " + strPrimeiroNumero);
     Console.WriteLine("O segundo número digitado foi: " + strSegundoNumero);
 
     Console.WriteLine();
+
+    bool primeiroNumeroVazio = string.IsNullOrEmpty(strPrimeiroNumero);
+    bool segundoNumeroVazio = string.IsNullOrEmpty(strSegundoNumero);
+
+    if (primeiroNumeroVazio == true || segundoNumeroVazio == true)
+    {
+       Console.WriteLine("Digite um número válido!");
+       Console.ReadLine();
+
+       continue; 
+    }
+
+    // if (string.IsNullOrEmpty(strPrimeiroNumero))
+
+    // if (strPrimeiroNumero == null || strSegundoNumero == null)
+    // {
+    //    Console.WriteLine("Digite um número válido!");
+    //    Console.ReadLine();
+
+    //    continue;
+    // }
 
     // int = número inteiro
     int primeiroNumero = Convert.ToInt32(strPrimeiroNumero);
